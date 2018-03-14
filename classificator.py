4f2c1aa723307348ec3dafa67bebaded21678853
David Medina Ospina <dameos1@gmail.com>
2018-03-14 18:28:06 -0500

    adding classficator

---
+++ classificator.py
@@ -0,0 +1,11 @@
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+import numpy as np
+
+dataX  = pd.read_csv('./dataset/x_values.csv')
+y = pd.read_csv('./dataset/y_values.csv') 
+
+clf = RandomForestClassifier(n_jobs=2, random_state=0)
+clf.fit(dataX, np.ravel(y))
+
+print(clf.predict(dataX))
